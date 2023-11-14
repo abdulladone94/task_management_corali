2b0e7c0390b8930bdd54076389dec9fce1974154
abdulladone94 <abdullaf@booleanlabs.biz>
2023-11-14 16:09:37 +0530

    be added time stamp, start and end dates

--- server/models/todoItems.js
+++ server/models/todoItems.js
@@ -1,14 +1,25 @@
 const mongoose = require('mongoose');
 
-const TodoItemSchema = new mongoose.Schema({
-  item: {
-    type: String,
-    required: true,
+const TodoItemSchema = new mongoose.Schema(
+  {
+    item: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('todo', TodoItemSchema);
